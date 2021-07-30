fd435ef3696f62b0f3c428df9efd62fc88ddef33
Matchan <matchan0727ahaha@gmail.com>
2021-07-30 16:51:43 +0900

    初期コミット

---
+++ src/main/java/jp/simplespace/simuanni/SimuANNI.java
@@ -0,0 +1,17 @@
+package jp.simplespace.simuanni;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimuANNI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
